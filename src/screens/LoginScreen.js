254a31e2b1a43d571ead408016a3e34a026cddf3
danilomartinezcanon15-af <danilomartinezcanon15@gmail.com>
2026-05-07 14:18:41 -0500

    Importaciones y configuraciones de Estados

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  Image,
+  SafeAreaView,
+  KeyboardAvoidingView,
+  Platform,
+  Alert,
+  ActivityIndicator,
+} from "react-native";
+import { Mail, Lock } from "lucide-react-native";
+import { API_URL } from "../config";
+
+const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  
